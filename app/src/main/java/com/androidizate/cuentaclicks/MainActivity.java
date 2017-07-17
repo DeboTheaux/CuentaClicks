0285c8b95d9ff1b031d863effcdbf2ae803622c0
DeboTheaux <debotheaux@gmail.com>
2017-07-17 20:44:11 -0300

    Contador de clicks

--- app/src/main/java/com/androidizate/cuentaclicks/MainActivity.java
+++ app/src/main/java/com/androidizate/cuentaclicks/MainActivity.java
@@ -4,21 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int clicks = 0;
+    TextView cantidadDeClicks;
+    Button botonClicks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.btn_sumar).setOnClickListener(new View.OnClickListener() {
+        cantidadDeClicks = (TextView) findViewById(R.id.tv_contador);
+        botonClicks = (Button) findViewById(R.id.btn_sumar);
+        botonClicks.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                EditText loTvContador = (EditText)findViewById(R.id.tv_contador);
-                Integer loValor = Integer.parseInt(loTvContador.getText().toString());
-                loTvContador.setText((loValor += 1).toString());
+                cantidadDeClicks.setText(sumarClicks());
             }
         });
+    }
+
+    private int sumarClicks() {
+        return clicks += 1;
     }
 }
